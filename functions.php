5970ddaf78d28cdef96db1b89654767a86d47a5f
GrowthWizard <dominik.stein@firmcatalyst.com>
2022-11-01 19:40:13 +0700

    1.0 First Release
    # Added
    
    - functions.php
    - index.php
    - READMe.md
    - style.css

---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php 
+
+/*
+* functions.php is only needed to enable WordPress Features we would like to use later for our REST API
+*/
+
+if ( function_exists(' blank-wordpress-theme-setup ')) {
+    function blank-wordpress-theme-setup() {
+        add_theme_support( 'automatic-feed-links' );
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'custom-logo' );
+        add_theme_support(
+			'custom-logo',
+			array(
+				'height'      => 256,
+				'width'       => 256,
+				'flex-height' => true,
+				'flex-width'  => true,
+				'header-text' => array( 'site-title', 'site-description' ),
+			)
+		);
+    }
+}
+
+endif;
+
+add_action( 'after_setup_theme', 'blank_setup' );
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+* index.php will be the backup and standard file for all post types
+* ...no Content will be shown in the frontend
+*/
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+
+</body>
+</html>
